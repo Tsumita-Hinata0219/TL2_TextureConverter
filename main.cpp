1e1373900240f245ed6251e19032005ebec18df3
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-10-07 14:24:40 +0900

    save

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@
 const char* PAUSE = "pause";
 
 
-int main(void) {
+int main(int argc, char* argv[]) {
 
-	std::cout << "Hello World!" << std::endl;
-	system(PAUSE);
+	// argcの数だけ繰り返す
+	for (int i = 0; i < argc; i++) {
+		// 文字列argvのi番目を表示
+		std::cout << argv[i] << std::endl;
+	}
 
 
+	std::cout << std::endl; // 改行
+	system(PAUSE); // 閉じる前にポーズしておく
 	return 0;
 }
