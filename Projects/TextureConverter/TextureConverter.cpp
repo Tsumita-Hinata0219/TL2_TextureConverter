e29cbffc643acd305f253e71fe6bf228ca4edb72
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2024-11-28 19:51:04 +0900

    テククチャコンバーターを作っていく

--- Projects/TextureConverter/TextureConverter.cpp
+++ Projects/TextureConverter/TextureConverter.cpp
@@ -1,1 +1,15 @@
 #include "TextureConverter.h"
+
+
+/// <summary>
+/// テクスチャをWICからDDSに変換する
+/// </summary>
+void TextureConverter::ConvertTextureWIC_To_DDS(const std::string& filePath)
+{
+	// ───── 1 テクスチャを読み込む
+
+
+	// ───── 2 DDS形式に変換して書き出す
+
+
+}
--- Projects/TextureConverter/TextureConverter.h
+++ Projects/TextureConverter/TextureConverter.h
@@ -1,5 +1,34 @@
 #pragma once
-class TextureConverter
-{
+
+#include <iostream>
+
+
+/* テクスチャコンバータクラス */
+class TextureConverter {
+
+public:
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	TextureConverter() = default;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~TextureConverter() = default;
+
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath"> ファイルパス </param>
+	void ConvertTextureWIC_To_DDS(const std::string& filePath);
+
+
+	
+private:
+
+
+
 };
 
